23c452b5f702a33542d09df1d66fd9291712a88c
Alex Psitos <apsitos08@gmail.com>
2022-03-25 08:49:51 -0400

    Finish test for podcasters parent children index spec, test passes

--- spec/features/podcasters/parent_children_index_spec.rb
+++ spec/features/podcasters/parent_children_index_spec.rb
@@ -13,5 +13,21 @@ RSpec.describe "the podcasters/podcaster_id/podcasts page", type: :feature do
     visit "/podcasters/#{podcaster_1.id}/podcasts"
 
     save_and_open_page
+
+    expect(page).to have_content(podcast_1.podcaster_name)
+    expect(page).to have_content(podcast_1.title)
+    expect(page).to have_content(podcast_1.topic)
+    expect(page).to have_content(podcast_1.length_in_minutes)
+    expect(page).to have_content(podcast_1.favorites)
+    expect(page).to have_content(podcast_2.podcaster_name)
+    expect(page).to have_content(podcast_2.title)
+    expect(page).to have_content(podcast_2.topic)
+    expect(page).to have_content(podcast_2.length_in_minutes)
+    expect(page).to have_content(podcast_2.favorites)
+    expect(page).to have_content(podcast_3.podcaster_name)
+    expect(page).to have_content(podcast_3.title)
+    expect(page).to have_content(podcast_3.topic)
+    expect(page).to have_content(podcast_3.length_in_minutes)
+    expect(page).to have_content(podcast_3.favorites)
   end
 end
